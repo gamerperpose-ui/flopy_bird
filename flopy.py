d79547ee553287fa76bf803402d310c0c0e8e881
Yash Coder 34 <gamerperpose@gmail.com>
2026-01-19 14:18:23 +0530

    Refactor Flopy game to include pipes and background

--- flopy.py
+++ flopy.py
@@ -1,20 +1,50 @@
-import pygame
+import pygame , random ,math
 pygame.init()
 
+def hit(ex,ey,bulletx,bullety):
+    distance = math.sqrt(math.pow(ex-bulletx +10,2) + math.pow(ey-bullety,2))
+    if distance < 40:
+        return True
+    else:
+        return False
+
+ex = random.randint(500, 800)
+ey  = random.randint(400,450)
+ex1 = ex   #random.randint(500, 800)
+ey1  = 0 
 width = 800
 heigth = 600
-x = 0
+x = 100
 y = 300
-movement = 100
+movement = 150
 size = 40
+exsize = 50
+eysize = 500
+ex1size = 50
+ey1size = random.randint(150, 200)
 down_movement = 0.2
+# ex = random.randint(500, 800)
+# ey  = random.randint(250,400)
+# ex1 = random.randint(500, 800)
+# ey1  = random.randint(0,250) 
 
 screen = pygame.display.set_mode((width,heigth))
 pygame.display.set_caption("ATHARVS FLOPY BIRD")
 
 
-flopy = pygame.image.load("image/flopy.png")
+flopy = pygame.image.load("images/flopy.png")
 flopy = pygame.transform.scale(flopy,(100,140))
+
+bg = pygame.image.load("images/back.png")
+bg = pygame.transform.scale(bg,(800,600))
+
+pipe1 = pygame.image.load("images/pipe.png")
+pipe1 = pygame.transform.scale(pipe1,(ex1size,ey1size))
+
+pipe2 = pygame.image.load("images/pipe2.png")
+pipe2 = pygame.transform.scale(pipe2,(exsize,eysize))
+
+
 
 game_on = True
 
@@ -30,14 +60,22 @@ while game_on:
                 pass
 
     y += down_movement
-    x += 0.01
+    ex -= down_movement
+    ex1 -= down_movement
+    if ex  <= 0:
+        ex =random.randint(800, 900) 
+    if ex1  <= 0:
+        ex1 = ex
     if y >= 550:
         y = 0
     if x >= 750:
         x=0    
-    screen.fill((255, 255 ,255))
+    screen.blit(bg,(0,0))
     screen.blit(flopy,(x , y))
-    # pygame.draw.rect(screen,(255, 0 ,0),( x, y, size, size))
+    screen.blit(pipe1,(ex1,ey1))
+    screen.blit(pipe2,(ex,ey))
+    # pygame.draw.rect(screen,(0, 0 ,0),( ex,ey, exsize, eysize))
+    # pygame.draw.rect(screen,(255, 0 ,0),( ex1,ey1, ex1size, ey1size))
     pygame.display.update()
 
 
